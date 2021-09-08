7c3bec67519205b69a6db097afe4dcd8e0f70f6a
Michael Werneke <mike.werneke@gmail.com>
2021-09-08 13:13:16 -0500

    routing

--- src/components/Example.js
+++ src/components/Example.js
@@ -4,15 +4,18 @@ import React, { useRef } from "react";
 import Pill from "./pill";
 import "./example.css"
 import useScreenRecorder from "use-screen-recorder";
-import {saveAs} from "file-saver";
+import { saveAs } from "file-saver";
 import axios from 'axios'
 import { nanoid } from "nanoid";
-import {useDispatch} from 'react-redux'  //**Added */
+import { useDispatch, useSelector } from 'react-redux'  //**Added */
 import FetchDetail from "./FetchDetail";
+import { useHistory } from 'react-router-dom';
 
 
 const Example = () => {
   const dispatch = useDispatch(); //**Added */
+  // const ticketDetailReducer = useSelector((store) => store.ticketDetailReducer); //TESTING
+  const history = useHistory();
   const {
     startRecording,
     pauseRecording,
@@ -32,11 +35,11 @@ const Example = () => {
 
   const handleUpload = async () => {
     const key = `${nanoid()}.webm`
-
-    const {data} = await axios.post('http://localhost:5522/api/storage/presignedUrl', {
+    // const title = `${FetchDetail()}` //TESTING
+    const { data } = await axios.post('http://localhost:5522/api/storage/presignedUrl', {
       key,
     })
-    const {url} = data
+    const { url } = data
 
     await axios({
       url,
@@ -48,71 +51,74 @@ const Example = () => {
       },
       onUploadProgress: (e) => {
         const progress = e.loaded / e.total
-        console.log({progress})
+        console.log({ progress })
       },
     })
 
     alert(`Upload done! ${key}`)
 
-      //NEED TO ACTIVATE ON UPLOAD
-      
-     dispatch({   //**ADDED */
-       type: 'POST_VIDEO',
-       payload: key
-     })   
-      
-    
-    
+    //NEED TO ACTIVATE ON UPLOAD
+
+    dispatch({   //**ADDED */
+      type: 'POST_VIDEO',
+      payload: key
+    })
+    history.push('/library')
 
   }
 
-  return (
-    
-    <div id="container">
-       <FetchDetail/>
-      {/* <Navbar /> */}
-      
-      <div className="wrapper">
-        <div className="pills">
-          <Pill style={{backgroundColor: status === 'recording'?'red':'inherit' }} title="Status" value={status} />
-          {/* <Pill
+
+
+
+
+
+return (
+
+  <div id="container">
+    <FetchDetail />
+    {/* <Navbar /> */}
+
+    <div className="wrapper">
+      <div className="pills">
+        <Pill style={{ backgroundColor: status === 'recording' ? 'red' : 'inherit' }} title="Status" value={status} />
+        {/* <Pill
             style={{ flexGrow: 1 }}
             title="Blob URL"
             value={blobUrl || "Waiting..."}
           /> */}
-        </div>
+      </div>
 
-        <div>
-          <video 
-          style={{width:'100%'}}
-            ref={videoRef}
-            src={blobUrl}
-            // poster={process.env.PUBLIC_URL + "/poster.png"}
-            controls
-            autoPlay
-          />
-        </div>
+      <div>
+        <video
+          style={{ width: '100%' }}
+          ref={videoRef}
+          src={blobUrl}
+          // poster={process.env.PUBLIC_URL + "/poster.png"}
+          controls
+          autoPlay
+        />
+      </div>
 
-        <div className="buttons">
-          {(status === "idle" || status === "error") && (
-            <button onClick={startRecording}>Start recording</button>
-          )}
-         
-          {(status === "recording"   || status === "paused") && (
-            <button onClick={stopRecording}>Stop recording</button>
-          )}
-          
-          {(status === "recording" || status === "paused") && (
-            <button
-              onClick={() =>
-                status === "paused" ? resumeRecording() : pauseRecording()
-              }
-            >
-              {status === "paused" ? "Resume recording" : "Pause recording"}
-            </button>
-          )}
-          {status === "stopped" && (
-            <>
+      <div className="buttons">
+        {(status === "idle" || status === "error") && (
+          <button onClick={startRecording}>Start recording</button>
+        )}
+
+        {(status === "recording" || status === "paused") && (
+          <button onClick={stopRecording}>Stop recording</button>
+        )}
+
+        {(status === "recording" || status === "paused") && (
+          <button
+            onClick={() =>
+              status === "paused" ? resumeRecording() : pauseRecording()
+            }
+          >
+            {status === "paused" ? "Resume recording" : "Pause recording"}
+          </button>
+        )}
+        {status === "stopped" && (
+          <>
             <button
               onClick={() => {
                 resetRecording();
@@ -123,13 +129,13 @@ const Example = () => {
             </button>
             <button onClick={handleDownload}>Download</button>
             <button onClick={handleUpload}>Upload</button>
-            </>
-          )}
-        </div>
+          </>
+        )}
       </div>
-
-    
     </div>
-  );
+
+
+  </div>
+);
 };
 export default Example;
